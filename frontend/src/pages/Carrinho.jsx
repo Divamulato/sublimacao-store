28fb90927ecd74ab12a45bb327cc09b137349356
Sergio Graca <sergiofgraca@gmail.com>
2026-06-08 18:09:51 -0300

    Altera cores da pagina carrinho

--- frontend/src/pages/Carrinho.jsx
+++ frontend/src/pages/Carrinho.jsx
@@ -1,6 +1,12 @@
 export default function Carrinho() {
   return (
-    <div style={{ padding: "40px" }}>
+    <div
+      style={{
+        padding: "40px",
+        color: "#000",
+        minHeight: "100vh"
+      }}
+    >
       <h1>🛒 Carrinho</h1>
 
       <p>Seu carrinho está vazio.</p>
