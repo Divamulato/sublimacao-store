a04c4d7b693535f874a795c40231ece15111718c
Sergio Graca <sergiofgraca@gmail.com>
2026-06-08 13:46:09 -0300

    Aumenta limite de upload para imagens

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({
+  extended: true,
+  limit: "10mb"
+}));
 
 /* =========================
    🔵 LISTAR PRODUTOS
