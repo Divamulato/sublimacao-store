eb90ccdb18744cdbf094a8db714e5e90525e47bc
Sergio Graca <sergiofgraca@gmail.com>
2026-06-15 21:08:42 -0300

    corrige imports duplicados

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import Home from "../pages/Home";
 import Produtos from "../pages/Produtos";
